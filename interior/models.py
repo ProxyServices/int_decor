0cc37127ae09c16254bbaf2081d4c53fec73836b
DennisIrimu <lyricaldnyt@gmail.com>
2019-07-22 23:19:02 +0300

    Initial commit plus blog post models

---
+++ interior/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
